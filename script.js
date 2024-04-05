bf50625eab4f6c6027f38381b8765ceb5f8fb6a3
prathm888 <prthmshingle@gmail.com>
2024-04-05 21:50:18 +0530

    java script
    java script

---
+++ script.js
@@ -0,0 +1,52 @@
+// console.log("i am a student");
+
+// var name=prompt("I am taking input");
+// console.log(name);
+
+// for(let i=1;i<=10;i++){
+// console.log(i*2);
+// }
+
+// let n=1;
+// while(n<=10){
+//     console.log(n);
+//     n++;
+// // }
+
+// let n=prompt("give me number");
+// if (n%2==0){
+//     console.log("I am even");
+// }
+// else{
+//     console.log("I am odd");
+// }
+
+
+// let marks=prompt("Enter ur marks");
+// if(marks>80 && marks<90){
+//     console.log("A garde");
+// }
+
+// else if (marks>70 && marks<=80){
+//     console.log("B garde");
+// }
+ 
+// else if (marks>60 && marks<=70){
+//     console.log("c garde");
+// }
+//  else{
+//     console.log(" D grade");
+//  }
+
+
+// let fname=prompt("give fname");
+// let lname=prompt("give lname");
+// let msg="hello"+fname+" "+lname+"your are using js !!"
+// alert(msg);
+
+// const favmovie=" 50 shades of grey";
+// let guess=prompt (" guess my fav movie");
+// while((guess!=favmovie) && (guess="quit")){
+// guess=prompt("wrong Ans!! please try again");
+// }
+
